e1eec7855ed485a84107161273c7acc3f0d7a051
Eric Grieser <eric@eric-Aspire-5542.(none)>
2013-09-23 22:16:11 -0500

    Initial Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Barcade::Application.config.secret_key_base = '124f9842c636e9bf6610a4edef77dc54de5f76897447baad516ef288497b529c3d52c8d31f5b345f2e77e99dfe06ffead69d67549acb377765fa0bc9d0b23b4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Barcade::Application.config.session_store :cookie_store, key: '_Barcade_session'
